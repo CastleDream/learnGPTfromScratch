c7be9b00a27b1aa552eb59ceaec281b6ee261281
CastleDream <hs8023hfp@gmail.com>
2026-06-15 21:55:48 +0800

    update video1_1:13

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+- [learnGPTfromScratch](#learngptfromscratch)
+  - [0. 环境说明](#0-环境说明)
+  - [1. 文件结构说明](#1-文件结构说明)
+
 # learnGPTfromScratch
+
+## 0. 环境说明
+关键的版本信息：
++ python: 3.9.5
++ 
+
+
+## 1. 文件结构说明
---
+++ code/a_utils.py
@@ -0,0 +1,34 @@
+from graphviz import Digraph
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
+    
+    for n in nodes:
+        # print(n, str(id(n))) # Value(data=-6.0) 140638430115392
+        # print(f"Node ID: {id(n)}, Label repr: {repr(n.label)}, Data: {n.data}")
+        dot.node(name=str(id(n)), label = "{%s|data %.4f|grad %.4f}" % (n.label, n.data, n.grad), shape='record')
+        # 创造了 * + 这样的伪节点，以及对应的连接
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
